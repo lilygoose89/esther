137bf32e2613aea64ad5dca7c8559d80fba191f7
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2020-02-19 14:31:33 +0700

    services modal

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,15 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  //click anywhere on the modal div (which includes the overlay) to exit the modal
+  $(".modal").click(function(){
+    $(".modal").removeClass("show");
+  });
+  //click on .button-service and call the modal that matches the id
+  $(".button-service").click(function(){
+    var id = $(this).attr("id");
+
+    $(".modal#"+id+"Modal").addClass("show");
+
+  });
 
 
-});
+  //smooth scrolling better because rachel
+  $("a[href^='#']").click(function () {
+    var link = this.hash.substr(1);
+    var section = $('*[id=' + link + ']');
 
-//smooth scrolling better because rachel
-$('a[href^="#"]').on('click',function () {
-  var link = this.hash.substr(1);
-  var section = $('*[id=' + link + ']');
+    $('html,body').animate({
+      scrollTop: section.offset().top
+    }, 800);
+    return false;
+  });
 
-  $('html,body').animate({
-    scrollTop: section.offset().top - 5
-  }, 800);
-  return false;
 });
